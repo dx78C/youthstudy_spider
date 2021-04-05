d27275ffc159c0f7b23bb837d53838f9ab3c7b22
colflip <colflip@88.com>
2021-04-05 21:51:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# 🚀️youthStudy完成页面
+
+> 使用者有责任和义务对自己的行为负责。
+> 本项目仅供学习交流使用，严禁用于其他用途! For learning and communication only, other use is strictly prohibited！
+> 作者不承担任何法律责任！The author assumes no legal liability！
+
+青年大学习获取最新完成页面，并发到指定邮箱。
+
+## 🎉️ 使用方法
+
+将配置信息填写在data.json中：
+
+```
+"user": "学号+姓名+",
+"dir": "下载目录",
+"smtpserver": "邮箱smtp服务器",
+"sender": "发送邮箱",
+"password": "授权码",
+"receiver": "接收邮箱1,接收邮箱2",
+"username": ""
+```
+
+运行main
+
+## ❤️感谢
+
+[QiYi92](https://github.com/QiYi92/Youth_Learning_Reptile)
+
+[zhouyumin](https://github.com/zhouyumin/qndxx)
---
+++ main.py
@@ -0,0 +1,142 @@
+import requests
+import re
+import bs4
+from bs4 import BeautifulSoup
+import os
+import smtplib
+import glob as gb  # 导入glob模块
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+import json
+from lxml import etree
+
+headers = {
+    'user - agent': 'Mozilla / 5.0(WindowsNT10.0;Win64;x64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 80.0.3987.116Safari / 537.36'
+}
+DATAFILENAME = "data.json"  # 数据文件的名称
+
+
+def get_first_url(url):  # 得到第一个url，即每一篇文章的url，结果是未遍历的
+    res_1 = requests.get(url=url, headers=headers)
+    html_1 = res_1.text
+    first_url = re.findall('<li.*?<a.*?"(//w.*?search)"', html_1, re.S)
+    # first_url = re.findall('<li.*?<a.*?"(//www.bilibili.com/read/cv*)"', html_1, re.S)
+    return first_url
+
+
+def get_second_url(url):  # 得到第二个url，即文章中每个图片的url，结果是未遍历的
+    res_2 = requests.get(url=url, headers=headers)
+    html_2 = res_2.text
+    soup = bs4.BeautifulSoup(html_2, 'html.parser')
+    picture_list = soup.select('.img-box img')
+    soup = BeautifulSoup(html_2, 'lxml')
+    title = soup.title.text
+    str1 = '第'
+    str2 = '期'
+    start = title.index(str1);
+    end = title.index(str2);
+    print(title[start:end + 1])
+    return picture_list, title[start:end + 1]
+
+
+def download_picture(url, dir, num1, i):  # 下载图片
+    res_3 = requests.get(url=url, headers=headers)
+    picture_data = res_3.content
+    picture_name = 'img{}_{}.jpg'.format(num1, i)
+    picture_path = dir + picture_name
+    with open(picture_path, 'wb') as f:
+        f.write(picture_data)
+
+
+# https://www.jianshu.com/p/f6ac9e997ef5
+def sendEmail(dataGroup, subject):
+    # 定义相关数据,请更换自己的真实数据
+    smtpserver = dataGroup["smtpserver"]
+    sender = dataGroup["sender"]
+    receiver = dataGroup["receiver"]
+    username = dataGroup["username"]
+    password = dataGroup["password"]
+
+    msg = MIMEMultipart()
+
+    msg['Subject'] = Header(subject, 'utf-8')
+    msg['From'] = sender
+    receivers = receiver
+    toclause = receivers.split(',')
+    msg['To'] = ",".join(toclause)
+    # print("send email:",msg['To'])
+    i = 0
+
+    img_path = gb.glob(dataGroup["dir"] + "*.jpg")
+    body = '"""' + '<h3>共' + str(len(img_path)) + '三张图片</h3></br>'
+    for path in img_path:
+        imgId = 'image' + str(i)
+        fp = open(path, 'rb')
+        images = MIMEImage(fp.read())
+        fp.close()
+        images.add_header('Content-ID', imgId)
+        msg.attach(images)
+        body = body \
+               + '<img src="cid:' + imgId + '">'
+        i += 1
+    body = body + \
+           '"""'
+    mail_body = MIMEText(body, _subtype='html', _charset='utf-8')
+    msg.attach(mail_body)
+
+    # 登陆并发送邮件
+    try:
+        smtp = smtplib.SMTP()
+        ##打开调试模式
+        # smtp.set_debuglevel(1)
+        smtp.connect(smtpserver)
+        smtp.login(username, password)
+        smtp.sendmail(sender, toclause, msg.as_string())
+    except:
+        print("邮件发送失败！！")
+    else:
+        print("邮件发送成功")
+    finally:
+        smtp.quit()
+
+
+def haveDataFile():
+    path = os.getcwd()
+    filelist = os.listdir(path)
+    for file in filelist:
+        if file == DATAFILENAME:
+            return True
+    return False
+
+
+# https://blog.csdn.net/qq_44921056/article/details/113398597
+if __name__ == '__main__':
+    if haveDataFile() == False:
+        print("[{}]".format(time.strftime(
+            "%Y-%m-%d %H:%M:%S", time.localtime())), "未读取到数据，新建数据文件,请到目录下的data.json配置信息")
+    else:
+        dataFile = open(DATAFILENAME, "rb")
+        dataGroup = json.load(dataFile)
+        dataGroup = dataGroup[1]
+        # 创建文件夹
+        dir = dataGroup["dir"]
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+
+        base_url = 'https://search.bilibili.com/article?keyword='
+        # insert=input()
+        insert = '青年大学习'
+        base_url = base_url + insert
+        fist_urls = get_first_url(base_url)
+        # num1 = 1
+        first_url = 'https:' + fist_urls[0]
+        second_url, title = get_second_url(first_url)
+        for i in range(len(second_url)):
+            picture_urls = second_url[i].get('data-src')
+            picture_url = 'https:' + picture_urls
+            download_picture(picture_url, dir, 1, i)
+        # num1 += 1
+        user = dataGroup["user"]
+        sendEmail(dataGroup, user + title)
